01c5fbe5188bb6b466121aedc1ab8d7f7e42dc3c
mari <sermarajraj35@gmail.com>
2022-08-03 03:05:03 -0800

    javascript-learning

---
+++ addition/script.js
@@ -0,0 +1,16 @@
+let a = 25;
+let b = 25;
+alert(a + b)
+console.log(a + b);
+
+
+var c = parseInt(prompt("first value:"))
+var d = parseInt(prompt("second value:"))
+console.log("first value:",c);
+console.log("second value:",d);
+console.log("Total value:", c + d);
+
+let e = 25;
+let f = 25;
+alert(a + b)
+document.write(e + f);
