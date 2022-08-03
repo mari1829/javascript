9f307ed6517fe124bf7d09f248167e27f3630ea5
mari <sermarajraj35@gmail.com>
2022-08-03 03:14:39 -0800

    javascript.

---
+++ area of circle/script.js
@@ -0,0 +1,4 @@
+const buy=3.14;
+let r = parseInt(prompt("give the radius value:"))
+let area = ((r ** 2) *buy)
+document.write(area);
---
+++ area of triangle/script.js
@@ -0,0 +1,7 @@
+let half=0.5;
+var birth=parseInt(prompt("please give the birth value:"));
+console.log("birth value:",birth);
+var height=parseInt(prompt("please give the height value:"));
+console.log("height value:",height);
+let triangle=(half*birth*height)
+console.log("triangle area:",triangle);
