db7842d4e006624ce15ef3b9313bc6aa29946518
mari <sermarajraj35@gmail.com>
2022-08-04 02:49:26 -0800

    javascript practice

---
+++ swaping/script.js
@@ -0,0 +1,30 @@
+// let a=1;
+// let b=2;
+// let c=a;
+// a=b;
+// b=c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+let a=1;
+let b=2;
+let c=3;
+let d=4;
+let e;
+e=a;
+a=b;
+b=c;
+c=d;
+d=e;
+console.log(a);
+console.log(b);
+console.log(c);
+// console.log(d);
+console.log(e);
+
+
+
+
+
+
