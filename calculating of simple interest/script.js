bde49a684a529b459a99091961025cb333daf140
mari <sermarajraj35@gmail.com>
2022-08-09 08:20:31 -0800

    js-program

---
+++ calculating of simple interest/script.js
@@ -0,0 +1,5 @@
+let p=parseInt(prompt("Give The P Value:"))
+let n=parseInt(prompt("Give The N Value:"))
+let r=parseInt(prompt("Give The R Value:"))
+let interest=(p*n*r/100)
+console.log(interest);
---
+++ compare two numbers and divisible by 7/script.js
@@ -0,0 +1,10 @@
+let a =parseInt(prompt("Give the first value:"))
+let b =parseInt(prompt("Give the second value:"))
+let c = (a * b)
+console.log(c);
+if (c % 7 == 0) {
+    console.log("This number divisible by 7:");
+}
+else{
+    console.log("This Number not  divisible by 7");
+}
