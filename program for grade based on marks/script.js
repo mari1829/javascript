26814c1e52299b0ac83d1e94f3836d26c15abeaf
mari <sermarajraj35@gmail.com>
2022-08-10 07:28:52 -0800

    java-script

---
+++ program for grade based on marks/script.js
@@ -0,0 +1,28 @@
+let a = parseInt(prompt("Give The Value:"));
+if (a > 91) {
+  console.log("Grade A:", a);
+}
+// else{
+else if (a > 75) {
+  console.log("Grade B:", a);
+} else if (a > 60) {
+  console.log("Grade C:", a);
+} else if (a > 45) {
+  console.log("Grade D:", a);
+} else if (a > 35) {
+  console.log("Grade E:", a);
+} else {
+  console.log("Fail");
+}
+
+// if(a>50){
+//     console.log("Grade D:",a);
+// }
+// if(a>35){
+//     console.log("grade E:",a);
+// }
+// if(a>0){
+//     console.log("fail:",a);
+// }
+
+// }
---
+++ program to find marriage eligibility/script.js
@@ -0,0 +1,7 @@
+let a = parseInt(prompt("Give The Value:"));
+
+if (a >= 29) {
+  console.log(a, "You Are Eligible");
+} else {
+  console.log("You Are Not Eligible");
+}
